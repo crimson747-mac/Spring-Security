b4e83550ddaab7a9264a1291dc4917ed6ab7ebfc
김남우 <crimson747@naver.com>
2021-08-05 08:40:51 +0900

    인증 필터 생성: AjaxAuthenticationFilter

---
+++ src/main/java/springsecuritystudy/security/secure/filter/AjaxLoginProcessingFilter.java
@@ -0,0 +1,2 @@
+package springsecuritystudy.security.secure.filter;public class AjaxLoginProcessingFilter {
+}
---
+++ src/main/java/springsecuritystudy/security/secure/token/AjaxAuthenticationToken.java
@@ -0,0 +1,2 @@
+package springsecuritystudy.security.secure.token;public class AjaxAuthenticationToken {
+}
