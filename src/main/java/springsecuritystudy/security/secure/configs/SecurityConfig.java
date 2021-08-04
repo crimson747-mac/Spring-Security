79d2270fd172ea61ecec50244a53848b340e37ab
김남우 <crimson747@naver.com>
2021-08-04 11:13:44 +0900

    커스텀 로그인 페이지 생성

--- src/main/java/springsecuritystudy/security/secure/configs/SecurityConfig.java
+++ src/main/java/springsecuritystudy/security/secure/configs/SecurityConfig.java
@@ -47,6 +47,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/messages").hasRole("MANAGER")
                 .antMatchers("/config").hasRole("ADMIN")
                 .anyRequest().authenticated()
-                .and().formLogin();
+            .and()
+                .formLogin().loginPage("/login")
+                .loginProcessingUrl("/login_proc")
+                .defaultSuccessUrl("/")
+                .permitAll();
     }
 }
